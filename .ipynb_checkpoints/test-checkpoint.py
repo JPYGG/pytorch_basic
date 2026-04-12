6c511ad800d405f87b7142de49f4788f134e3bc9
GanQinRen <1296528123@qq.com>
2026-04-12 23:05:59 +0800

    pytorch

---
+++ .ipynb_checkpoints/test-checkpoint.py
@@ -0,0 +1,6 @@
+import torch
+
+# 打印cuda设备, 显卡名字
+print(torch.cuda.get_device_name(0))
+
+
---
+++ test.py
@@ -0,0 +1,8 @@
+import torch
+
+# 打印cuda设备, 显卡名字
+print(torch.cuda.get_device_name(0))
+
+print(torch.tensor([6,2]))
+
+
